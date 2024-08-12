ec3e959fce2bac6993a353696e26e9c4aaad908c
Akshat <akshatvijayvergiya64@gmail.com>
2024-08-12 22:36:14 +0530

    Root component

--- src/components/root-component.tsx
+++ src/components/root-component.tsx
@@ -19,6 +19,9 @@ const RootLayout: React.FC<RootLayoutProps> = ({ children }) => {
       {pathname === "/login" ? <div className="grid min-h-screen w-full">{children}</div> :
         <>
         
+          <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
+                {children}
+          </main>
         </>
       }
     </>
