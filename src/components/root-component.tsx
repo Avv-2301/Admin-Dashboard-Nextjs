99569fd7da6758dac8aef044ad3fc1f8633143ba
Akshat <akshatvijayvergiya64@gmail.com>
2024-08-12 23:07:36 +0530

    Root component

--- src/components/root-component.tsx
+++ src/components/root-component.tsx
@@ -1,10 +1,23 @@
 import React, { ReactNode } from "react";
-import {
-  Sheet,
-  SheetContent,
-  SheetTitle,
-} from "@/components/ui/sheet";
+import Link from "next/link";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { Button } from "@/components/ui/button";
 import { useRouter } from "next/router";
+
+import {
+  Bell,
+  Gamepad2,
+  BookText,
+  CircleUser,
+  Home,
+  LineChart,
+  Menu,
+  Package,
+  Package2,
+  Search,
+  SquareTerminal,
+  Users,
+} from "lucide-react";
 
 type RootLayoutProps = {
   children: ReactNode;
@@ -18,9 +31,58 @@ const RootLayout: React.FC<RootLayoutProps> = ({ children }) => {
     <>
       {pathname === "/login" ? <div className="grid min-h-screen w-full">{children}</div> :
         <>
-        
+          <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+            <div className="hidden border-r bg-muted/40 md:block">
+              <div className="flex h-full max-h-screen flex-col gap-2">
+                <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
+                  <Link href="/" className="flex items-center gap-2 font-semibold">
+                  <Gamepad2 className="h-6 w-6"/>
+                    <span className="">BiGamer</span>
+                  </Link>
+                </div>
+                <div className="flex-1">
+                  <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
+                    {/* LINKS HERE FOR LARGE VIEW */}
+                  </nav>
+                </div>
+              </div>
+            </div>
+            <div className="flex flex-col">
+              <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
+              <Sheet>
+                  <SheetTrigger asChild>
+                    <Button
+                      variant="outline"
+                      size="icon"
+                      className="shrink-0 md:hidden"
+                    >
+                      <Menu className="h-5 w-5" />
+                      <span className="sr-only">Toggle navigation menu</span>
+                    </Button>
+                  </SheetTrigger>
+                  <SheetContent side="left" className="flex flex-col">
+                    <nav className="grid gap-2 text-lg font-medium">
+                    <Link
+                        href="#"
+                        className="flex items-center gap-2 text-lg font-semibold"
+                      >
+                        <Gamepad2 className="h-6 w-6" />
+                        <span className="sr-only">Acme Inc</span>
+                      </Link>
+                      <Link
+                      href={'/users'}
+                      className=""
+                      >
+                      
+                      </Link>
+                    </nav>
+                    </SheetContent>
+                </Sheet>
+              </header>
+            </div>
+          </div>
           <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
-                {children}
+            {children}
           </main>
         </>
       }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,11 @@
 import type { AppProps } from 'next/app';
 import '@/styles/globals.css';
 import RootLayout from '@/components/root-component';
- 
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <RootLayout>
-        <Component {...pageProps} />
-      </RootLayout>
-    </>
+    <RootLayout>
+      <Component {...pageProps} />
+    </RootLayout>
   );
 }
