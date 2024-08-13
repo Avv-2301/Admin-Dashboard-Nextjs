cc89272dc0f89bb2e9efbc87480014012e65aee8
Akshat <akshatvijayvergiya64@gmail.com>
2024-08-13 22:42:54 +0530

    Dashboard Page

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -13,65 +13,65 @@ import { User } from "lucide-react";
 const dashboard = () => {
   return (
     <div>
-      <div className="flex flex-row items-center justify-between space-y-2">
-        <h2 className="text-2xl font-bold tracking-tight">Dashboard</h2>
+  <div className="flex flex-row items-center justify-between">
+    <h2 className="text-2xl font-bold tracking-tight">Dashboard</h2>
+  </div>
+  <Tabs defaultValue="account" className="space-y-4 mb-4">
+    <TabsList>
+      <TabsTrigger value="account">Account</TabsTrigger>
+      <TabsTrigger value="password">Password</TabsTrigger>
+    </TabsList>
+    <TabsContent value="account">
+      <div className='flex flex-row gap-4 space-x-4  mt-4'>
+        <Card>
+          <CardHeader className="flex flex-row items-center space-y-0 pb-2 gap-4">
+            <CardTitle className="text-lg font-medium">
+              Total Users
+            </CardTitle>
+            <User className="ml-2 h-6 w-6" />
+          </CardHeader>
+          <CardContent>
+            <div className="text-2xl font-bold">50</div>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader className="flex flex-row items-center space-y-0 pb-2 gap-4">
+            <CardTitle className="text-lg font-medium">
+              Total Users
+            </CardTitle>
+            <User className="ml-2 h-6 w-6" />
+          </CardHeader>
+          <CardContent>
+            <div className="text-2xl font-bold">50</div>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader className="flex flex-row items-center space-y-0 pb-2 gap-4">
+            <CardTitle className="text-lg font-medium">
+              Total Users
+            </CardTitle>
+            <User className="ml-2 h-6 w-6" />
+          </CardHeader>
+          <CardContent>
+            <div className="text-2xl font-bold">50</div>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader className="flex flex-row items-center space-y-0 pb-2 gap-4">
+            <CardTitle className="text-lg font-medium">
+              Total Users
+            </CardTitle>
+            <User className="ml-2 h-6 w-6" />
+          </CardHeader>
+          <CardContent>
+            <div className="text-2xl font-bold">50</div>
+          </CardContent>
+        </Card>
       </div>
-      <Tabs defaultValue="account" className="space-y-4 mb-4">
-        <TabsList>
-          <TabsTrigger value="account">Account</TabsTrigger>
-          <TabsTrigger value="password">Password</TabsTrigger>
-        </TabsList>
-        <TabsContent value="account">
-          <div className='flex flex-row justify-between mt-4'>
-            <Card>
-              <CardHeader className="flex flex-row items-center space-y-0 pb-2 gap-4">
-                <CardTitle className="text-lg font-medium">
-                  Total Users
-                </CardTitle>
-                <User className="ml-2 h-4 w-4" />
-              </CardHeader>
-              <CardContent>
-                <div className="text-2xl font-bold">50</div>
-              </CardContent>
-            </Card>
-            <Card>
-              <CardHeader className="flex flex-row items-center space-y-0 pb-2 gap-4">
-                <CardTitle className="text-lg font-medium">
-                  Total Users
-                </CardTitle>
-                <User className="ml-2 h-4 w-4" />
-              </CardHeader>
-              <CardContent>
-                <div className="text-2xl font-bold">50</div>
-              </CardContent>
-            </Card>
-            <Card>
-              <CardHeader className="flex flex-row items-center space-y-0 pb-2 gap-4">
-                <CardTitle className="text-lg font-medium">
-                  Total Users
-                </CardTitle>
-                <User className="ml-2 h-4 w-4" />
-              </CardHeader>
-              <CardContent>
-                <div className="text-2xl font-bold">50</div>
-              </CardContent>
-            </Card>
-            <Card>
-              <CardHeader className="flex flex-row items-center space-y-0 pb-2 gap-4">
-                <CardTitle className="text-lg font-medium">
-                  Total Users
-                </CardTitle>
-                <User className="ml-2 h-4 w-4" />
-              </CardHeader>
-              <CardContent>
-                <div className="text-2xl font-bold">50</div>
-              </CardContent>
-            </Card>
-          </div>
-        </TabsContent>
-        <TabsContent value="password">Set A Password</TabsContent>
-      </Tabs>
-    </div>
+    </TabsContent>
+    <TabsContent value="password">Set A Password</TabsContent>
+  </Tabs>
+</div>
   );
 };
 
